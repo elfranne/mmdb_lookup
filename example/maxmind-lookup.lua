2888b387fbcc4406bd65f8c76f0e62d366f4318a
Elfranne <861038+elfranne@users.noreply.github.com>
2025-07-15 13:48:23 +0200

    wip

---
+++ example/maxmind-lookup.lua
@@ -0,0 +1,18 @@
+-- Source: https://github.com/elfranne/maxmind-lookup
+-- Copyright (C) 2025 Elfranne
+-- License: MIT
+
+local function http_request(src)
+  local s = core.tcp()
+  s:connect("127.0.0.1:10000")
+  s:send("GET /" .. src .. " HTTP/1.1\r\nHost: 127.0.0.1:10000\r\n\r\n")
+  local msg = s:receive("*l")
+  return msg
+
+end
+
+local function maxmindlookup(txn)
+  txn:set_var('txn.geoip_country', http_request(txn.f:src()))
+end
+
+core.register_action('maxmindlookup', { 'tcp-req', 'http-req' }, maxmindlookup, 0)
